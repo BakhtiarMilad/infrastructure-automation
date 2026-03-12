d8204502793df27878e3e3e8188451225aaf768f
Milad Bakhtiar <bakhtiar.milad@gmail.com>
2026-03-12 09:11:47 +0100

    format main

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "iac-project-tfstate-milad"
-    key = "state.tfstate"
-    region = "eu-central-1"
+    bucket  = "iac-project-tfstate-milad"
+    key     = "state.tfstate"
+    region  = "eu-central-1"
     encrypt = true
   }
 }
