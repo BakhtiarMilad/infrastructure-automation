9e1ce24f14838cfd18d9062bb8cfbeab0ee3f010
Milad Bakhtiar <bakhtiar.milad@gmail.com>
2026-03-14 12:05:42 +0000

    test ci for branch protection (#1)
    * test ci for branch protection
    
    * add chechov check
    
    * add chechov CLI log
    
    * solve checkov findings
    
    * format main
    
    * validate main

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+resource "kubernetes_namespace_v1" "weather_ns" {
+  metadata {
+    name = "weather-app-system"
+  }
+}
+
 terraform {
   backend "s3" {
     bucket  = "iac-project-tfstate-milad"
@@ -25,9 +31,10 @@ provider "kubernetes" {
 
 # --- 3. CONFIGMAP (DIE WETTER-KONFIGURATION) ---
 # Das simuliert die Einstellungen für Satelliten-Datenströme.
-resource "kubernetes_config_map" "weather_settings" {
+resource "kubernetes_config_map_v1" "weather_settings" {
   metadata {
-    name = "weather-config"
+    name      = "weather-config"
+    namespace = kubernetes_namespace_v1.weather_ns.metadata[0].name
   }
 
   data = {
@@ -44,7 +51,8 @@ resource "kubernetes_config_map" "weather_settings" {
 # Das Herzstück: Hier wird die Applikation definiert.
 resource "kubernetes_deployment" "nginx" {
   metadata {
-    name = var.app_name
+    name      = var.app_name
+    namespace = kubernetes_namespace_v1.weather_ns.metadata[0].name
   }
 
   spec {
@@ -61,17 +69,76 @@ resource "kubernetes_deployment" "nginx" {
         }
       }
       spec {
+        # security context auf Pod-Ebene (CKV_K8S_30)
+        security_context {
+          run_as_non_root = true
+          run_as_user     = 101 #standard-ID for the Nginx-User
+          fs_group        = 101
+        }
         # Hier binden wir die ConfigMap als virtuelles Laufwerk (Volume) ein
         volume {
           name = "config-volume"
           config_map {
-            name = kubernetes_config_map.weather_settings.metadata[0].name
+            name = kubernetes_config_map_v1.weather_settings.metadata[0].name
           }
         }
 
+        volume {
+          name = "nginx-cashe"
+          empty_dir {}
+        }
+        volume {
+          name = "nginx-run"
+          empty_dir {}
+        }
+
         container {
-          image = "nginx:latest"
+          image = "nginx:1.25.3@sha256:2bdc49f2f8ae1d8dbdb68ae9f5f48ef746c0721094031f6aa9ebcb726485749a"
           name  = "nginx-container"
+
+          liveness_probe {
+            http_get {
+              path = "/"
+              port = 80
+            }
+
+            initial_delay_seconds = 15
+            period_seconds        = 20
+            timeout_seconds       = 5
+            failure_threshold     = 3
+          }
+
+          readiness_probe {
+            http_get {
+              path = "/"
+              port = 80
+            }
+            initial_delay_seconds = 5
+            period_seconds        = 10
+          }
+
+          resources {
+            requests = {
+              cpu    = "100m" #100 milliCPUs (0.1 Kerne)
+              memory = "128Mi"
+            }
+
+            limits = {
+              cpu    = "500m" #500 milliCPUs (0.5 Kerne)
+              memory = "256MI"
+            }
+          }
+
+          #security context on container level
+          security_context {
+            read_only_root_filesystem  = true
+            run_as_non_root            = true
+            run_as_user                = 101
+            allow_privilege_escalation = false
+            capabilities {
+              drop = ["ALL"]
+            }
+          }
 
           port {
             container_port = 80
@@ -83,6 +150,15 @@ resource "kubernetes_deployment" "nginx" {
             mount_path = "/usr/share/nginx/html/wetter.json"
             sub_path   = "weather_config.json"
             read_only  = true
+          }
+          # Diese Mounts erlauben Nginx das Schreiben trotz Read-Only FS:
+          volume_mount {
+            name       = "nginx-cache"
+            mount_path = "/var/cache/nginx"
+          }
+          volume_mount {
+            name       = "nginx-run"
+            mount_path = "/var/run"
           }
         }
       }
